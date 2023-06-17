b335ae677652eb56509b1d87f5fab782d710463a
SamAska <samuel.asika0@gmail.com>
2023-06-17 04:09:26 -0700

    Newer

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /*
  * using put instead of print
  *
  */
-
-int main(void) 
+int main(void)
 { 
-	puts ("\"programming is like building a multilingual puzzle");
-       	return (0);
+puts("\"programming is like building a multilingual puzzle");
+return(0);
 }
