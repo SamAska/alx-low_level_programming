24ed4483f330add08fb2c4e82f560b924d6553a3
SamAska <samuel.asika0@gmail.com>
2023-06-17 04:17:09 -0700

    To print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /*
+ * main - printing
  * using put instead of print
  *
  */
 int main(void)
-{ puts ("\"programming is like building a multilingual puzzle");
-return(0);
+{ puts("\"programming is like building a multilingual puzzle");
+return (0);
 }
