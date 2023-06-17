9e6b6b3d3eb5ba06db1057f96561b8af32bf2f2b
SamAska <samuel.asika0@gmail.com>
2023-06-17 04:21:06 -0700

    bdkfb

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /*
- * main - printing
+ * main - Printing
  * using put instead of print
  *
+ * Return: Always 0 (Successful).
  */
 int main(void)
-{ puts("\"programming is like building a multilingual puzzle");
+{ puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
