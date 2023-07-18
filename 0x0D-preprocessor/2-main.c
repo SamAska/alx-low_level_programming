e8f4b687112366283e46e8a2fa38190b3acb157a
SamAska <samuel.asika0@gmail.com>
2023-07-17 21:11:11 -0700

    Preprocessor Task

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this command prints the name of the file
+ * Return: to ensure always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
