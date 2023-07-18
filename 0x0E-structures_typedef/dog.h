212537ac13e6a8b536cac2b41f7f621a1427cc07
SamAska <samuel.asika0@gmail.com>
2023-07-17 22:18:30 -0700

    First Task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - this is a dog's basic info
+ * @name: this is the first member
+ * @age: this is the second member
+ * @owner: this is the third member
+ * Description: this is a longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
